b1127b6f800c1bffeca6b6aefddd484b43329638
jfk86 <habiburrahman86@gmail.com>
2025-06-16 13:09:00 +0100

    Add complete Qaida Testing Dashboard - Core Components and Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AssessmentProvider } from './contexts/AssessmentContext';
+import { QaidaProvider } from './contexts/QaidaContext';
 import Header from './components/Header';
+import DashboardSelector from './components/DashboardSelector';
 import AssessmentPage from './pages/AssessmentPage';
+import QaidaRoadmap from './components/QaidaRoadmap';
+import QaidaAssessment from './components/QaidaAssessment';
 
 function App() {
   return (
     <AssessmentProvider>
-      <Router>
-        <div className="App">
-          <Header />
-          <Routes>
-            <Route path="/" element={<AssessmentPage />} />
-          </Routes>
-        </div>
-      </Router>
+      <QaidaProvider>
+        <Router>
+          <div className="App">
+            <Header />
+            <Routes>
+              <Route path="/" element={<DashboardSelector />} />
+              <Route path="/tajweed" element={<AssessmentPage />} />
+              <Route path="/qaida" element={<QaidaRoadmap />} />
+              <Route path="/qaida/level/:levelId" element={<QaidaAssessment />} />
+            </Routes>
+          </div>
+        </Router>
+      </QaidaProvider>
     </AssessmentProvider>
   );
 }
